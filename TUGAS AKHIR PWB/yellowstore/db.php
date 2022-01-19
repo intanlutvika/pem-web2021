0b60fb0530a8b35f875aa18caf47b158e4840cdb
Intan Lutvika <92045539+intanlutvika@users.noreply.github.com>
2022-01-20 00:36:46 +0700

    Delete TUGAS AKHIR PWB/yellowstore directory

--- TUGAS AKHIR PWB/yellowstore/db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$hostname = 'localhost';
-	$username = 'root';
-	$password = '';
-	$dbname   = 'yellowstore';
-
-	$conn = mysqli_connect($hostname, $username, $password, $dbname) or die ('Gagal terhubung ke database');
-?>
